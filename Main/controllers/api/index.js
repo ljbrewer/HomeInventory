9d4551337fde5aa322f58f2c62a19c56ea786d70
maxwell walin <maxwellwalin@gmail.com>
2021-09-25 14:24:45 -0700

    set up controllers to access homepage and login routes

---
+++ Main/controllers/api/index.js
@@ -0,0 +1,10 @@
+// const router = require('express').Router();
+// const userRoutes = require('./userRoutes');
+// const homeRoutes = require('./homeRoutes')
+// const assetRoutes = require('./assetRoutes');
+
+// router.use('/users', userRoutes);
+// router.use('/homes', homeRoutes);
+// router.use('/asset', assetRoutes);
+
+// module.exports = router;

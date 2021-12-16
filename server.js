377969ece5b6b339da0ee8b8878314fd93702b7d
maxwell walin <maxwellwalin@gmail.com>
2021-12-15 18:48:37 -0800

    removed ES6 imports due to error, formatted

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-import { join } from 'path';
+const path = require('path');
 require('dotenv').config();
-import express, { json, urlencoded, static } from 'express';
-import session, { Store } from 'express-session';
-import { create } from 'express-handlebars';
-import routes from './controllers';
-import helpers from './utils/helpers';
-import sequelize, { sync } from './config/connection';
-const SequelizeStore = require('connect-session-sequelize')(Store);
-import _ from 'underscore';
-import morgan from "morgan-body";
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const _ = require('underscore');
+const morgan = require("morgan-body");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const hbs = create({ helpers }); // .create({ helpers })
+const hbs = exphbs.create({ helpers }); // .create({ helpers })
 
 const sesh = {
     secret: 'CHANGEME',
@@ -30,14 +30,14 @@ app.use(session(sesh));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(json());
-app.use(urlencoded({ extended: true }));
-app.use(static(join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 morgan(app);
 
 app.use(routes);
 
-sync({ force: false }).then(() => {
+sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
 });
